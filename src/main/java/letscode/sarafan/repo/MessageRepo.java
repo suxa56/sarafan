d202d25847c68a2c32e01f42bf6e96e17a0ff572
suxa56 <suxa5637@mail.ru>
2021-03-20 23:05:53 +0500

    DB LombokFix Repo JsonFormat JsonView

---
+++ src/main/java/letscode/sarafan/repo/MessageRepo.java
@@ -0,0 +1,8 @@
+package letscode.sarafan.repo;
+
+import letscode.sarafan.domain.Message;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MessageRepo extends JpaRepository<Message, Long> {
+
+}
