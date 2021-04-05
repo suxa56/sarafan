5e03a6886b3342b4e41ca3e173e4d018c951d78d
suxa56 <suxa5637@mail.ru>
2021-04-05 11:31:10 +0500

    sss

---
+++ src/main/java/letscode/sarafan/config/WebSecurityConfig.java
@@ -0,0 +1,26 @@
+package letscode.sarafan.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .mvcMatchers("/").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .oauth2Login()
+                .and()
+                .csrf().disable();
+    }
+
+
+}
---
+++ src/main/java/letscode/sarafan/repo/UserDetailsRepo.java
@@ -0,0 +1,9 @@
+package letscode.sarafan.repo;
+
+import letscode.sarafan.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserDetailsRepo extends JpaRepository<User, String> {
+
+
+}
